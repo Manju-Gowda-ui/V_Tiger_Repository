4aa0e350b8a8c23be6726ceda8aa1f9aad7a8918
HOME UEFI 21H2 <HOME UEFI 21H2@DESKTOP-2IB6IFP>
2025-08-11 15:59:12 +0530

    LoginPage has newly added to my project hence it has been added to Global Repo

---
+++ V_Tiger_Practise/src/main/java/com/Vtiger/CRM/Generic/DatabaseUtility/Login_Page.java
@@ -0,0 +1,14 @@
+package com.Vtiger.CRM.Generic.DatabaseUtility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.Test;
+
+public class Login_Page {
+	
+	@FindBy(id = "Nanesta")
+	private WebElement git;
+
+}
